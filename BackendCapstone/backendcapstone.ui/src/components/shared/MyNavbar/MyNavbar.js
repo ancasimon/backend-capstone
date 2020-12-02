6f845695bbfc347715b855f458f9645a33b39c2d
Anca Simon <ancarsimon@gmail.com>
2020-12-02 01:22:16 -0600

    wip on navbar

---
+++ BackendCapstone/backendcapstone.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+    render() {
+        return (
+            <div className="MyNavbar">
+                Navbar
+            </div>
+        )
+    }
+}
+
+export default MyNavbar;
