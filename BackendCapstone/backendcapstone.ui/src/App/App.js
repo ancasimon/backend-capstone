478cd1f5886e88c6cad157f49f8fc2753369ddd1
Anca Simon <ancarsimon@gmail.com>
2020-12-02 01:16:52 -0600

    auth for frontend

--- BackendCapstone/backendcapstone.ui/src/App/App.js
+++ BackendCapstone/backendcapstone.ui/src/App/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+
+import fbConnection from '../helpers/data/connection';
+
+import Login from '../components/pages/Login/Login';
+
 import './App.scss';
+ 
+fbConnection();
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
     Main Page
+    <Login></Login>
       </div>
     )
   }
