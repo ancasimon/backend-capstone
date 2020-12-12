5436979df62f31cb1d36a54311a5615744419f10
Anca Simon <ancarsimon@gmail.com>
2020-12-12 13:46:53 -0600

    added user on Home page

--- BackendCapstone/backendcapstone.ui/src/components/pages/Home/Home.js
+++ BackendCapstone/backendcapstone.ui/src/components/pages/Home/Home.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
+import usersData from '../../../helpers/data/usersData';
+
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    user: {},
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser = () => {
+    usersData.getSingleUser()
+      .then((userResponse) => {
+        this.setState({ user: userResponse.data });
+      })
+      .catch((error) => console.error('Could not get user data.', error));
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <div className="Home">
-        <h2 className="pageTitle">Welcome to Unlimited!</h2>
+        {
+          user ? <h2 className="pageTitle">Welcome to Unlimited, {user.firstName}!</h2> : <h2 className="pageTitle">Welcome to Unlimited!</h2>
+        }
       </div>
     );
   }
