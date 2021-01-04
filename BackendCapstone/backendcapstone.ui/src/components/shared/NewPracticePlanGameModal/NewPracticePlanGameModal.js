d446c9cffdfa1ef08496e44996976e48f01ece57
Anca Simon <ancarsimon@gmail.com>
2021-01-04 02:07:45 -0600

    wip

---
+++ BackendCapstone/backendcapstone.ui/src/components/shared/NewPracticePlanGameModal/NewPracticePlanGameModal.js
@@ -0,0 +1,211 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import {
+  Button,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Table,
+} from 'reactstrap';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import { parseISO, format } from 'date-fns';
+import Swal from 'sweetalert2';
+import PropTypes from 'prop-types';
+
+import gamesData from '../../../helpers/data/gamesData';
+import practicePlanGamesData from '../../../helpers/data/practicePlanGamesData';
+import practicePlansData from '../../../helpers/data/practicePlansData';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './NewPracticePlanGameModal.scss';
+
+class NewPracticePlanGameModal extends React.Component {
+  static propTypes = {
+    gameId: PropTypes.number.isRequired,
+    practicePlanId: PropTypes.number.isRequired,
+    closeModal: PropTypes.func.isRequired,
+    getPracticePlanDetails: PropTypes.func,
+  }
+
+  state = {
+    selectedGame: {},
+    selectedPlan: {},
+    practiceGameName: '',
+    practiceDate: new Date(),
+    practiceNotes: '',
+    practiceCompleted: false,
+    practicePlanStartDate: new Date(),
+    practicePlanEndDate: new Date(),
+  }
+
+  getGameForm = (e) => {
+    const { gameId } = this.props;
+    gamesData.getGameById(gameId)
+      .then((singleGameResponse) => {
+        this.setState({
+          selectedGame: singleGameResponse.data,
+          practiceGameName: singleGameResponse.data.name,
+        });
+      });
+  };
+
+  getPracticePlanInfo = () => {
+    const { practicePlanId } = this.props;
+    practicePlansData.getSinglePracticePlan(practicePlanId)
+      .then((ppResponse) => {
+        this.setState({
+          selectedPlan: ppResponse.data,
+          practicePlanStartDate: parseISO(ppResponse.data.startDate),
+          practicePlanEndDate: parseISO(ppResponse.data.endDate),
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.getGameForm();
+    this.getPracticePlanInfo();
+  }
+
+  changePracticeGameName = (e) => {
+    e.preventDefault();
+    this.setState({ practiceGameName: e.target.value });
+  }
+
+  changePracticeDate = (date) => {
+    this.setState({ practiceDate: date });
+  }
+
+  changePracticeNotes = (e) => {
+    e.preventDefault();
+    this.setState({ practiceNotes: e.target.value });
+  }
+
+  changePracticeCompleted = (e) => {
+    e.preventDefault();
+    this.setState({ practiceCompleted: e.target.checked });
+  }
+
+  validationAlertPracticeGameDate = () => {
+    Swal.fire('Game date must be between practice plan start and end dates.');
+  }
+
+  savePracticePlanGame = (e) => {
+    e.preventDefault();
+    const {
+      practiceGameName,
+      practiceDate,
+      practiceNotes,
+      practiceCompleted,
+      selectedGame,
+      practicePlanStartDate,
+      practicePlanEndDate,
+    } = this.state;
+    const { gameId, practicePlanId, getPracticePlanDetails } = this.props;
+    if (practiceGameName == '') {
+      this.setState({ practiceGameName: selectedGame.name });
+    }
+    if (practiceDate == '') {
+      this.setState({ practiceDate: new Date() });
+    }
+    if (practiceDate.getTime() < practicePlanStartDate.getTime() || practiceDate.getTime() > practicePlanEndDate.getTime()) {
+      this.validationAlertPracticeGameDate();
+    } else {
+      const newPracticePlanGame = {
+        name: this.state.practiceGameName,
+        practiceDate: this.state.practiceDate,
+        userNotes: practiceNotes,
+        isCompleted: practiceCompleted,
+        practicePlanId,
+        gameId,
+      };
+      practicePlanGamesData.createNewPracticePlanGame(newPracticePlanGame)
+        .then((newPpgResponse) => {
+          this.props.closeModal();
+        })
+        .catch((error) => console.error('Could not add the game selected to your practice plan.', error));
+    }
+  }
+
+  render() {
+    const {
+      practiceGameName,
+      practiceDate,
+      practiceNotes,
+      practiceCompleted,
+      selectedPlan,
+    } = this.state;
+
+    return (
+      <div className='NewPracticePlanGameModal'>
+        {/* code for modal about the game selected below: */}
+          <ModalHeader toggle={this.toggleGameFormModal}>Add <span className='italics'>{this.state.selectedGame.name}</span> to Plan <span className='italics'>{selectedPlan.name}</span></ModalHeader>
+          <ModalBody>
+            <div>
+            <Form>
+              <FormGroup>
+                <Label for="practiceName">Practice Game Name (customize as needed)</Label>
+                <Input
+                  type="input"
+                  name="practiceName"
+                  value={practiceGameName}
+                  id="practiceName"
+                  onChange={this.changePracticeGameName}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="practiceDate">Practice Date</Label>
+                <DatePicker
+                  selected={this.state.practiceDate}
+                  name="practiceDate"
+                  value={practiceDate}
+                  id="practiceDate"
+                  onChange={this.changePracticeDate}
+                  dateFormat={'MM-dd-yyyy'}
+
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="userNotes">Notes</Label>
+                <Input
+                  type="textarea"
+                  name="userNotes"
+                  id="userNotes"
+                  placeholder="Anything specific to remember for this practice?"
+                  value={practiceNotes}
+                  onChange={this.changePracticeNotes}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    id="isCompleted"
+                    value={practiceCompleted}
+                    onChange={this.changePracticeCompleted}
+                  />{' '}
+                  Already completed this practice?
+                </Label>
+              </FormGroup>
+            </Form>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button className="mainButtons p-2" onClick={this.savePracticePlanGame}>Save Game</Button>
+            <Button className="mainButtons p-2" onClick={this.props.closeModal}>Cancel</Button>
+          </ModalFooter>
+      </div>
+    );
+  }
+}
+
+export default NewPracticePlanGameModal;
--- README.md
+++ README.md
@@ -11,18 +11,20 @@ A full-stack CRUD application built with C#, ASP .NET Core MVC, SQL database, HT
 ![Games Page](./BackendCapstone/backendcapstone.ui/src/images_backend_capstone/GamesPage.PNG)
 
 ## Tools & Technologies
-API, ASP .NET Core MVC, Axios, Azure (for SQL database and deployment), Bootstrap, C#, CSS, E6 Modules, Firebase (for authentication and hosting), FreeLogoDesign, Github (for version control and project management), HTML5, JavaScript, Lucidchart, moqups, React, React Router, reactstrap, Saas, SQL, SSMS
+API, ASP .NET Core MVC, Axios, Azure (for SQL database and deployment), Bootstrap, C#, CSS, E6 Modules, Firebase (for authentication and hosting), FreeLogoDesign, Github (for version control and project management), HTML5, JavaScript, Lucidchart, moqups, React, React Router, reactstrap, Saas, SQL, SSMS, sweetalert2
 
 ## Features
 1. Users can view a database of games that they can utilize to enhance music practices with their young students and their metadata. 
-1. Users can filter the games by the age and instrument they are intended for and by the amount of prework required.
-1. Users can acces a game details page, whre they see all the details of a game, including prework steps, instructions, and the source whom we can thank for each idea!
+1. Users can filter the games by keywords as well as the age and instrument the games are intended for and by the amount of prework required.
+1. Users can access a game details page, whre they see all the details of a game, including prework steps, instructions, and the source whom we can thank for each idea!
 1. Users can register on the web site and log in to take advantage of
 additional features.
-1. Authenticated users can create practice plans and select specific games form the database for days included in the practice plan. 
+1. Authenticated users can create practice plans and select specific games from the database for days included in the practice plan. 
 1. Authenticated users can keep track of children's progress by marking mark games on practice plans as completed and keeping notes about how individual games worked for them.
 1. Authenticated users can edit and delete practice plans.
-1. Authenticated users can view their profile.
+1. Authenticated users can add new games to the database and edit the games they have added.
+1. Authenticated users can delete games they added to the database as long as they have not yet been used in a practice plan.
+1. Authenticated users can view their profile, which includes a list of the awesome contributions each of them has made to this database.
 
 ## Deployment
 This app has been deployed with Firebase for the frontend, Azure for the backend, and using an Azure SQL database. You can find it here: https://backendcapstone-bccf6.web.app/home.
