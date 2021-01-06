045f98fb3991d5863f8444b29a93d6c3c355c75f
Anca Simon <ancarsimon@gmail.com>
2021-01-05 22:41:12 -0600

    added 3 basic unit tests

--- BackendCapstone/BackendCapstone.Tests/UnitTestSetGamesFilterValues.cs
+++ BackendCapstone/BackendCapstone.Tests/UnitTestSetGamesFilterValues.cs
@@ -7,14 +7,19 @@ namespace BackendCapstone.Tests
 {
     public class UnitTestSetGamesFilterValues
     {
+        // Arrange (Given, Context)
+
+        // Act (When)
+
+        // Assert (Then)
+
 
         [Fact]
         public void When_user_does_not_select_an_age_filter_then_all_age_ids_get_passed_to_sql_query()
         {
             // Arrange (Given, Context)
             // Given these parameters:
-            // the user ID:
-            List<int> ageFilterValuesSelected = null;
+            List<int> ageFilterValuesSelected = new List<int>();
 
             List<int> currentAges = new List<int>() { 1, 2, 3, 4, 5 };
 
@@ -24,20 +29,53 @@ namespace BackendCapstone.Tests
             // and using methods from this repository:
             var gameWithMetadataRepo = new GameWithMetadataRepository();
 
-
-            // Act (When) -- ANCA: THIS IS WHERE I CALL A METHOD FROM A REPO FILE???? 
-            // IF I JUST WANTED TO CHECK THE USER ID - should I write a new method in the Repo for it??? Even if I don't need it for the app itself??
+            // if I am passing an empty list to this method, then it should return the full list:
             var solution = gameWithMetadataRepo.EnsureValidFilterValuesAge(ageFilterValuesSelected);
 
             // Assert (Then)
             Assert.Equal(expectedResult, solution);
         }
 
-        // Arrange (Given, Context)
+        [Fact]
+        public void When_user_does_not_select_an_instrument_filter_then_all_instrument_ids_get_passed_to_sql_query()
+        {
+            // Arrange (Given, Context)
+            // Given these parameters:
+            List<int> instrumentFilterValuesSelected = new List<int>();
 
-        // Act (When)
+            List<int> currentInstruments = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
 
-        // Assert (Then)
+            // and this expected result:
+            List<int> expectedResult = currentInstruments;
 
+            // and using methods from this repository:
+            var gameWithMetadataRepo = new GameWithMetadataRepository();
+
+            var solution = gameWithMetadataRepo.EnsureValidFilterValuesInstrument(instrumentFilterValuesSelected);
+
+            // Assert (Then)
+            Assert.Equal(expectedResult, solution);
+        }
+
+        [Fact]
+        public void When_user_does_not_select_a_preworkLevel_filter_then_all_preworkLevel_ids_get_passed_to_sql_query()
+        {
+            // Arrange (Given, Context)
+            // Given these parameters:
+            List<int> preworkLevelFilterValuesSelected = new List<int>();
+
+            List<int> currentPreworkLevels = new List<int>() { 1, 2, 3, 4 };
+
+            // and this expected result:
+            List<int> expectedResult = currentPreworkLevels;
+
+            // and using methods from this repository:
+            var gameWithMetadataRepo = new GameWithMetadataRepository();
+
+            var solution = gameWithMetadataRepo.EnsureValidFilterValuesPreworkLevel(preworkLevelFilterValuesSelected);
+
+            // Assert (Then)
+            Assert.Equal(expectedResult, solution);
+        }
     }
 }
