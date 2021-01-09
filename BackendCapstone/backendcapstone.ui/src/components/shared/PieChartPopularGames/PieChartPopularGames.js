37c0bf2cd4cbb2f120bd616bd9b6e0a7836c7d6d
Anca Simon <ancarsimon@gmail.com>
2021-01-09 00:52:35 -0600

    changed to responsive pie chart

--- BackendCapstone/backendcapstone.ui/src/components/shared/PieChartPopularGames/PieChartPopularGames.js
+++ BackendCapstone/backendcapstone.ui/src/components/shared/PieChartPopularGames/PieChartPopularGames.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import {
-  PieChart, Pie, Legend, Tooltip,
+  ResponsiveContainer, PieChart, Pie, Legend, Tooltip,
 } from 'recharts';
 import { Link } from 'react-router-dom';
 
@@ -8,14 +8,13 @@ import gamesData from '../../../helpers/data/gamesData';
 
 class Example extends PureComponent {
   state = {
-    data01: [],
-    data02: [],
+    data: [],
   }
 
   getMostPopularGames = () => {
     gamesData.getMostPopularGames()
       .then((mostPopularGamesResponse) => {
-        this.setState({ data01: mostPopularGamesResponse, data02: mostPopularGamesResponse });
+        this.setState({ data: mostPopularGamesResponse });
       })
       .catch((error) => console.error('Could not get most popular games.', error));
   }
@@ -25,14 +24,23 @@ class Example extends PureComponent {
   }
 
   render() {
-    const { data01, data02 } = this.state;
+    const { data } = this.state;
 
     return (
-      <PieChart width={400} height={400}>
-        <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#286086" label={(entry) => entry.name} />
-        <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
-        <Tooltip />
-      </PieChart>
+    // <PieChart width={400} height={400}>
+    //   <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#286086" label={(entry) => entry.name} />
+    //   <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+    //   <Tooltip />
+    // </PieChart>
+
+      <div style={{ width: '100%', height: 300 }}>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie dataKey="value" data={data} fill="#286086" label={(entry) => entry.name} />
+        </PieChart>
+      </ResponsiveContainer>
+      </div>
+
     );
   }
 }
